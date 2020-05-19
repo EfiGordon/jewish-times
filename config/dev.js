cd5dc087f3f2cc2ec2d30c6a55528201389f4341
Efi Gordon <efigordon90@gmail.com>
2020-05-19 21:51:31 +0300

    change keys

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,14 @@
 //dev.js dont commit
 
-module.exports = {
-    startDate: '2020-05-18',
-    endDate: '2020-12-31',
-    googleClientSecret: 'eXghAfPdN5C_ESAdz0MMfhMV',
-    mongoURI: 'mongodb://efi:EfiGordon90@ds061374.mlab.com:61374/my-career-page-dev',
-    cookieKey: 'htrhrth45rh534h45h54h45h45h534wh4545g',
-    jwtSecretKey: 'secret_this_should_be_longer',
-    githubAccessToken: '8a00ca62a4c5101ba6e944da295e183b6ea9890e'
+const Dev = () => {
+    console.log('Dev..');
+    return {
+        startDate: '2020-05-18',
+        endDate: '2020-05-30',
+        googleClientSecret: 'eXghAfPdN5C_ESAdz0MMfhMV',
+        mongoURI: 'mongodb://efi:EfiGordon90@ds061374.mlab.com:61374/my-career-page-dev',
+        cookieKey: 'htrhrth45rh534h45h54h45h45h534wh4545g',
+        jwtSecretKey: 'secret_this_should_be_longer',
+        githubAccessToken: '8a00ca62a4c5101ba6e944da295e183b6ea9890e'
+    }
 }
