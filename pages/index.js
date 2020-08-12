41baad8b3a5ea8e5008b328274d94f27adb929b6
Efi Gordon <efigordon90@gmail.com>
2020-08-12 21:14:10 +0300

    add adsense script to main page

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
     <HomeLayout titleName="Zmanim">
       <Head>
         <title>{siteTitle}</title>
+        <script data-ad-client="ca-pub-9268139755114912" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </Head>
       <section className={utilStyles.headingMd}>
         <Typography>
